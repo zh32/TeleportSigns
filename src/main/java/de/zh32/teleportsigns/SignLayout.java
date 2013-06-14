b720958b0c1748b9b98ef4e6f379b9131233332b
zh32 <geese.thomas@gmx.de>
2013-06-14 15:35:02 +0200

    Revert "hopefully fixed a npe"
    This reverts commit 212c7985296a22d64bed6a8a862094139cf50daa.

--- src/main/java/de/zh32/teleportsigns/SignLayout.java
+++ src/main/java/de/zh32/teleportsigns/SignLayout.java
@@ -37,7 +37,7 @@ class SignLayout {
                         }
                         motdCount++;
                     } else {
-                        line = line.replace("%motd%", " ");
+                        line = line.replace("%motd%", "");
                     }
                 }
             }
