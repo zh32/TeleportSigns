04a940718c153bebedc089f8d5761613a822a1c7
zh32 <devnull@zh32.de>
2016-04-10 13:54:13 +0200

    Update server ping to minecraft 1.9

--- src/main/java/de/zh32/teleportsigns/server/status/StatusResponse.java
+++ src/main/java/de/zh32/teleportsigns/server/status/StatusResponse.java
@@ -1,35 +1,43 @@
 package de.zh32.teleportsigns.server.status;
 
-import java.util.List;
 import lombok.Data;
 
+import java.util.List;
+
 /**
- *
  * @author zh32 <zh32 at zh32.de>
  */
 @Data
 public class StatusResponse {
-    private String description;
-    private Players players;
-    private Version version;
-    private String favicon;
-    private int time;
-	
-    @Data
-    public class Players {
-        private int max;
-        private int online;
-        private List<Player> sample;     
-    }
-    @Data
-    public class Player {
-        private String name;
-        private String id;
-        
-    }
-    @Data
-    public class Version {
-        private String name;
-        private int protocol;
-    }
+
+	private Description description;
+	private Players players;
+	private Version version;
+	private String favicon;
+	private int time;
+
+	@Data
+	public class Description {
+		private String text;
+	}
+
+	@Data
+	public class Players {
+		private int max;
+		private int online;
+		private List<Player> sample;
+	}
+
+	@Data
+	public class Player {
+		private String name;
+		private String id;
+
+	}
+
+	@Data
+	public class Version {
+		private String name;
+		private int protocol;
+	}
 }
