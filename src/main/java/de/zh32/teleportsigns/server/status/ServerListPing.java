2d31ad86b2d56a0dd34560658a6f8b4238e9525f
zh32 <devnull@zh32.de>
2016-04-16 19:32:09 +0200

    Add ping support for minecraft 1.7-1.8. #14

--- src/main/java/de/zh32/teleportsigns/server/status/ServerListPing.java
+++ src/main/java/de/zh32/teleportsigns/server/status/ServerListPing.java
@@ -23,9 +23,9 @@ public class ServerListPing {
 			StatusResponse response = queryHandler.doStatusQuery();
 			connection.disconnect();
 			server
-					.setMotd(response.getDescription().getText())
-					.setMaxPlayers(response.getPlayers().getMax())
-					.setPlayersOnline(response.getPlayers().getOnline())
+					.setMotd(response.getDescription())
+					.setMaxPlayers(response.getOnlinePlayers())
+					.setPlayersOnline(response.getMaxPlayers())
 					.setOnline(true);
 		} catch (Exception ex) {
 			server.setOnline(false);
